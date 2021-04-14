d046ac8b9a33f3310d641a66c6d50956725d2c91
Mateus Santos <mateus.sa94@hotmail.com>
2021-04-14 10:38:55 -0300

    criei o arquivo index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+
+	<head>
+		<meta charset="utf-8">
+	</head>
+	
+	<body>
+		<section>
+			<h1></h1>
+		</section>
+		
+		<section>
+			<form>
+				<div>
+					<input type="text" placeholder="Nome" >
+				</div>
+				
+				<div>
+					<input type="text" placeholder="E-mail" >
+				</div>
+			
+				<div>
+					<input type="password" placeholder="Senha" >
+				</div>
+				
+				<div>
+					<input type="submit" value="Cadastrar" >
+				</div>
+			</form>
+		</section>
+	</body>
+</html>
