baa9a38ecf3c847a44aa3448d928a9f3b3b6eebf
Mateus Santos <mateus.sa94@hotmail.com>
2021-04-20 12:12:28 -0300

    modifiquei os arquivos index.php e styles.css e adicionei o arquivo lighslider.css

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="css/lightslider.css"/>
+		
+		<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+		<script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
 	</head>
 	
 	<body>
@@ -34,6 +38,12 @@
 				
 				<div >
 					<input class="bt-cadastro" type="submit" value="Cadastrar" >
+				</div>
+				
+				<div class="social">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					<a href="#"><i class="fab fa-twitter"></i></a>
+					<a href="#"><i class="fab fa-instagram"></i></a>
 				</div>
 				</nav>
 			</form>
